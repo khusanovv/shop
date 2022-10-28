1034f9d871d2dfa6120ed911a55747c41348b97b
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2022-10-28 23:30:30 +0500

    Add classList

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
 const cart = document.querySelector(".shop-cart");
-const imgApi = document.querySelector(".imgapi");
-
       fetch("https://fakestoreapi.com/products")
-        .then((data) => data.json() )
+        .then((data) => data.json())
         .then((data2) =>  {
           data2.forEach((element) => {
-            console.log(element.image);
-            const pEl = document.createElement("p")
-            const img = document.createElement("img")
-            const div = document.createElement("div")
-            console.log(img);
+            console.log(element);
+
+            // Create Element
+            const pEl = document.createElement("h3"),
+             desc = document.createElement("p"),
+             img = document.createElement("img"),
+             div = document.createElement("div"),
+             btn = document.createElement("button");
+
             img.src = element.image;
             img.alt = "Image"
-            pEl.innerHTML = element.title;
+            pEl.innerHTML = "Price: " + element.price
+            desc.innerHTML = "Desc: " + element.description.split("").splice(1, 70).join("") + "..."
+            btn.innerHTML = "U"
+
+            // console.log(element.description.split("").splice(1, 80).join(""));
             
-            cart.appendChild(div)
-            div.appendChild(img);
+            cart.appendChild(div);
+            div.appendChild(img );
             div.appendChild(pEl);
+            div.appendChild(desc)
+            div.appendChild(btn)
+
+
+            // Add class 
             div.classList.add("cart__item")
+            desc.classList.add("desc")
            
           });
         });
