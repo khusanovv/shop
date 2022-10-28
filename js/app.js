391445a4b2306e4e1e602035ade10f727d78e365
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2022-10-29 01:55:31 +0500

    delete api add

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
+let api = "https://fakestoreapi.com/products"
+let options = {
+    method: "DELETE"
+    
+}
 const cart = document.querySelector(".shop-cart");
-      fetch("https://fakestoreapi.com/products")
+      fetch("https://fakestoreapi.com/products" )
         .then((data) => data.json())
         .then((data2) =>  {
           data2.forEach((element) => {
-            console.log(element);
+            // console.log(element);
 
             // Create Element
             const pEl = document.createElement("h3"),
-             desc = document.createElement("p"),
-             img = document.createElement("img"),
-             div = document.createElement("div"),
-             btn = document.createElement("button");
+                desc = document.createElement("p"),
+                img = document.createElement("img"),
+                div = document.createElement("div"),
+                btn = document.createElement("button");
+                
 
             img.src = element.image;
             img.alt = "Image"
@@ -21,15 +27,46 @@ const cart = document.querySelector(".shop-cart");
             // console.log(element.description.split("").splice(1, 80).join(""));
             
             cart.appendChild(div);
+            
+            div.innerHTML = `<button data-target-id="${element.id}">delete</button>`
             div.appendChild(img );
             div.appendChild(pEl);
             div.appendChild(desc)
-            div.appendChild(btn)
+            
+            
+
+            // function myFunction() {
+            //     let text = "Press a button!\nEither OK or Cancel.";
+            //     if (confirm(text) == true) {
+            //       text = "Cart o`chirildi";
+            //     } else {
+            //       text = "You canceled!";
+            //     }
+            //     document.getElementById("demo").innerHTML = text;
+            //   }
 
 
             // Add class 
             div.classList.add("cart__item")
             desc.classList.add("desc")
+
+            // Delete
+            
            
+            
+            
+        })
+        const shopCart = document.querySelector(".shop-cart");
+            shopCart.addEventListener('click', (e)=>{
+              // console.log(e.target);
+              if(e.target.hasAttribute("data-target-id")){
+                  // console.log(e.target.dataset.productId);
+                  const elementId = e.target.dataset.targetId
+                //   console.log(elementId);
+                  fetch(`https://fakestoreapi.com/products/${elementId}` ,{
+                      method: "DELETE"
+                  })
+                  .then(res => console.log(res.json()))
+              }
           });
         });
